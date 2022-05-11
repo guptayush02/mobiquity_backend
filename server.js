558807561a718c0b824477a4d9ee4dc4eccef375
ayush <guptayush02@gmail.com>
2022-05-12 02:05:10 +0530

    add public folder to run frontend and set default route to public folder in server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express")
 const cors = require("cors")
+const path = require("path")
 
 const app = express()
 
 const corsOptions = {
   origin: "http://localhost:8080"
 }
+
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.static('public/'));
 
 app.use(cors(corsOptions))
 
@@ -14,6 +19,14 @@ app.use(express.json())
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }))
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  });
+});
 
 const db = require("./models")
 db.sequelize.sync()
