8bede5284fbed199ceb9aba4aba828b28001ff39
ayush <guptayush02@gmail.com>
2022-05-12 13:21:42 +0530

    add frontend build

--- app/controllers/fileController.js
+++ app/controllers/fileController.js
@@ -1,22 +1,48 @@
 const fileDao = require("../dbQueries/fileDao")
 const { sendResponse, createJwt, comparePassword, createPassword } = require("../utils/utils")
+const AWS = require('aws-sdk')
+const fs = require('fs')
 
 module.exports.upload = async(req, res) => {
   try {
-    const { url, title } = req.body
+    const { uploadedFile, title, description, uploadedFileName, file } = req.body
     const { id } = req.user
-    if (!url || !title) {
-      return sendResponse(res, 404, `File not found`)
+    if (!uploadedFile || !title || !uploadedFileName) {
+      return sendResponse(res, 400, `File not found`)
     }
-    const body = {
-      ...req.body,
-      userId: id
+    AWS.config.update({ accessKeyId: "AKIAQCOSDMZIKJOLB2V6", secretAccessKey: "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" })
+    let s3 = new AWS.S3()
+    const buf = Buffer.from(uploadedFile, 'base64');
+    const newFile = uploadedFileName.replace(/ /g, "").split(".")
+    const fileContent = fs.readFileSync(file);
+    const params = {
+      Bucket: 'fileuploadsystem',
+      Key: `${newFile[0]}.${newFile[1]}`,
+      Body: fileContent
+    }
+    const result = await uploadFile(s3, params)
+    let body = {
+      title,
+      description,
+      userId: id,
+      url: result
     }
     await fileDao.create(body)
     return sendResponse(res, 200, `File upload successfully`)
   } catch (err) {
-    return sendResponse(res, 404, `Error cought in fileController [upload] function catch block ${error}`)
+    return sendResponse(res, 404, `Error cought in fileController [upload] function catch block ${err}`)
   }
+}
+
+const uploadFile = (s3, params) => {
+  return new Promise((resolve, reject) => {
+    s3.upload(params, (err, data) => {
+      if (err) {
+        reject(err)
+      }
+      resolve(data.Location)
+    })
+  })
 }
 
 module.exports.getFiles = async(req, res) => {
@@ -26,7 +52,7 @@ module.exports.getFiles = async(req, res) => {
     const files = await fileDao.findFilesByUserId(where)
     return sendResponse(res, 200, `user uoloaded files` , {files})
   } catch (err) {
-    return sendResponse(res, 404, `Error cought in fileController [getFiles] function catch block ${error}`)
+    return sendResponse(res, 404, `Error cought in fileController [getFiles] function catch block ${err}`)
   }
 }
 
@@ -37,7 +63,7 @@ module.exports.delete = async(req, res) => {
     await fileDao.delete(where)
     return sendResponse(res, 200, `File successfully deleted`)
   } catch (err) {
-    return sendResponse(res, 404, `Error cought in fileController [delete] function catch block ${error}`)
+    return sendResponse(res, 404, `Error cought in fileController [delete] function catch block ${err}`)
   }
 }
 
